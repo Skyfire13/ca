5b4bcf2b2fbcaeab6ada1922624df64186035a20
Skyfire13 <alanjamesfuller@gmail.com>
2023-04-22 11:22:24 -0500

    Randomize board only fills center

--- rulestring_ca.py
+++ rulestring_ca.py
@@ -32,63 +32,74 @@ from os import system
 
 
 
-def newBoard(rows, cols):
+def newBoard(rows: int, cols: int) -> list:
     board = np.zeros((rows, cols), dtype=int)
     return board
 
 
-def printBoard(board):
+def printBoard(board: list) -> None:
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
                 print("  ", end="")
             else:
-                print("🮙 ", end="")
+                print("X ", end="")
         print()
 
 
-   
-
-def randomizeBoard(board):
+def randomizeBoard(board: list) -> list:
     tempBoard = board
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            tempBoard[i][j] = random.choice([0,1])
+
+    half_size = int(len(board) / 2)
+    center_buffer = int(len(board) / 4)
+    
+    for i in range(half_size):
+        for j in range(half_size):
+            tempBoard[i + center_buffer][j + center_buffer] = random.choice([0,1])
     return tempBoard
 
 
-def fillBoard(board):
+def fillBoard(board: list) -> list:
     tempBoard = board
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            tempBoard[i][j] = 1
+    half = int(len(board)) / 2
+    
+    for i in range(len(board) / 2):
+        for j in range(len(board) / 2):
+            tempBoard[i + half][j + half] = 1
     return tempBoard
 
 
-def countNeighbors(board, r, c):
+def countNeighbors(board: list, rows: int, cols: int) -> int:
     count = 0
-    for i in range(r-1, r+2):
-        for j in range(c-1, c+2):
-            if (i<0 or j<0) or (i>len(board)-1 or j>len(board[0])-1) or (i==r and j==c):
+    for i in range(rows - 1, rows + 2):
+        for j in range(cols - 1, cols + 2):
+            if (i < 0 or j < 0)\
+                    or (i > len(board) - 1\
+                    or j > len(board[0]) - 1)\
+                    or (i == rows and j == cols):
                 continue
             if board[i][j] == 1:
                 count +=1
     return count
 
 
-def processRulestring(rulestring):
+def processRulestring(rulestring: str) -> str | str:
     born = False;
     survive = False;
 
     # Arrays of numbers for each action
-    born_values = rulestring[:rulestring.find('s')].strip('b')
-    survive_values = rulestring[rulestring.find('s'):].strip('s')
+    #born_values = rulestring[:rulestring.find('s')].strip('b')
+    #survive_values = rulestring[rulestring.find('s'):].strip('s')
+
+    born_values = rulestring[:rulestring.find(':')]
+    survive_values = rulestring[rulestring.find(':'):].strip(':')
+
     return born_values, survive_values
 
     
 
 # Remember to use matplotlib.animate
-def iterate(board, rulestring):
+def iterate(board: list, rulestring: str) -> list:
 
     born, survive = processRulestring(rulestring)
 
@@ -105,22 +116,41 @@ def iterate(board, rulestring):
     return tempBoard
    
 
-def main():
+def main() -> None:
     #rows = int(input("Number of rows: "))
     #cols = int(input("Number of columns: "))
 
-    rows = 50
-    cols = 50
-    rulestring = 'b35678s5678'
+    rows = 100
+    cols = 100
+    iterations = 0
+    # rulestring = '5:23457'
+    #rulestring = '45:23457'
+    #rulestring = '345:23457'
+    #rulestring = '345:5'
+    rulestring = '3456:5'
+    # rulestring = '28:012345678' <- Frost
+    rulestring = '18:345678'
+
+    # No rulestring with birth values exclusively lower than 4
+    # cannot grow. There would need to be an empty cell at the
+    # edge of the spawn radius that four neighbors, which is
+    # impossible on edges
+
     board = newBoard(rows, cols)
     board = randomizeBoard(board)
 
     #main game loop
     while True:
-        system('clear')
+        system('printf "\033c"')
         printBoard(board)
+        print("\n Iterations: " + str(iterations))
         board = iterate(board, rulestring)
-        time.sleep(0.025)
+        iterations += 1
+        #time.sleep(0.025)
+
+       # if keyboard.is_pressed("q"):
+       #     print("Exiting...")
+       #     break
 
 
 main()
